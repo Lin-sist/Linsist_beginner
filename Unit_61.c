c11d671159c000764f8b882c9ac7cd604c949cc1
Lin-sist <linitsolong@gmail.com>
2024-11-12 20:15:25 +0800

    Add files via upload
    C程序设计，第六章《数组》课后练习，第一题。

---
+++ Unit_61.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+int main ()
+{
+	int i,j;
+	int a[100];
+
+	for (i = 0; i < 100; i++)
+	{
+		a[i] = 1;
+	}
+
+	a[0] = 0;
+	a[1] = 0;
+
+	for (i = 2; i < sqrt(100); i++)
+	{
+		if (a[i] == 1)
+		{
+			for (j = i * i; j < 100; j += i)
+			{
+				a[j] = 0;
+			}
+		}
+	}
+
+	for (i = 2; i < 100; i++)
+	{
+		if (a[i] == 1)
+		{
+			printf ("%4d",i);
+		}
+	}
+	return 0;
+}
